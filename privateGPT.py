ad3735e70fd72f021bccaab39fceb61027420f13
0x7d0 <30491318+0x7d0@users.noreply.github.com>
2023-06-16 12:32:21 +0200

    flask

--- privateGPT.py
+++ privateGPT.py
@@ -1,4 +1,7 @@
-#!/usr/bin/env python3
+from flask import Flask, jsonify, request, abort
+from flask_cors import CORS
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from dotenv import load_dotenv
 from langchain.chains import RetrievalQA
 from langchain.embeddings import HuggingFaceEmbeddings
@@ -6,77 +9,56 @@ from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.vectorstores import Chroma
 from langchain.llms import GPT4All, LlamaCpp
 import os
-import argparse
-import time
 
 load_dotenv()
 
-embeddings_model_name = os.environ.get("EMBEDDINGS_MODEL_NAME")
-persist_directory = os.environ.get('PERSIST_DIRECTORY')
+# Environment Variables
+EMBEDDINGS_MODEL_NAME = os.getenv("EMBEDDINGS_MODEL_NAME")
+PERSIST_DIRECTORY = os.getenv('PERSIST_DIRECTORY')
+MODEL_TYPE = os.getenv('MODEL_TYPE')
+MODEL_PATH = os.getenv('MODEL_PATH')
+MODEL_N_CTX = os.getenv('MODEL_N_CTX')
+MODEL_N_BATCH = int(os.getenv('MODEL_N_BATCH', 8))
+TARGET_SOURCE_CHUNKS = int(os.getenv('TARGET_SOURCE_CHUNKS', 4))
+# Ensure required environment variables are set
+REQUIRED_ENV_VARS = ['EMBEDDINGS_MODEL_NAME', 'PERSIST_DIRECTORY', 'MODEL_TYPE', 'MODEL_PATH', 'MODEL_N_CTX']
+for var in REQUIRED_ENV_VARS:
+    if not os.getenv(var):
+        raise Exception(f"Missing required environment variable {var}")
 
-model_type = os.environ.get('MODEL_TYPE')
-model_path = os.environ.get('MODEL_PATH')
-model_n_ctx = os.environ.get('MODEL_N_CTX')
-model_n_batch = int(os.environ.get('MODEL_N_BATCH',8))
-target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
+app = Flask(__name__)
+CORS(app)
+limiter = Limiter(key_func=get_remote_address)
+limiter.init_app(app)
 
-from constants import CHROMA_SETTINGS
+embeddings = HuggingFaceEmbeddings(model_name=EMBEDDINGS_MODEL_NAME)
+db = Chroma(persist_directory=PERSIST_DIRECTORY, embedding_function=embeddings)
+retriever = db.as_retriever(search_kwargs={"k": TARGET_SOURCE_CHUNKS})
 
-def main():
-    # Parse the command line arguments
-    args = parse_arguments()
-    embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
-    db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
-    retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
-    # activate/deactivate the streaming StdOut callback for LLMs
-    callbacks = [] if args.mute_stream else [StreamingStdOutCallbackHandler()]
-    # Prepare the LLM
-    match model_type:
-        case "LlamaCpp":
-            llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks, verbose=False)
-        case "GPT4All":
-            llm = GPT4All(model=model_path, n_ctx=model_n_ctx, backend='gptj', n_batch=model_n_batch, callbacks=callbacks, verbose=False)
-        case _default:
-            print(f"Model {model_type} not supported!")
-            exit;
-    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents= not args.hide_source)
-    # Interactive questions and answers
-    while True:
-        query = input("\nEnter a query: ")
-        if query == "exit":
-            break
-        if query.strip() == "":
-            continue
+callbacks = [StreamingStdOutCallbackHandler()]
 
-        # Get the answer from the chain
-        start = time.time()
+# Prepare the LLM
+if MODEL_TYPE == "LlamaCpp":
+    llm = LlamaCpp(model_path=MODEL_PATH, n_ctx=MODEL_N_CTX, n_batch=MODEL_N_BATCH, callbacks=callbacks, verbose=False)
+elif MODEL_TYPE == "GPT4All":
+    llm = GPT4All(model=MODEL_PATH, n_ctx=MODEL_N_CTX, backend='gptj', n_batch=MODEL_N_BATCH, callbacks=callbacks, verbose=False)
+else:
+    raise Exception(f"Model {MODEL_TYPE} not supported!")
+
+qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
+
+@app.route('/ask', methods=['POST'])
+@limiter.limit("60/minute")  # Limit to 60 requests per minute
+def ask():
+    query = request.json.get('query')
+    if not query:
+        abort(400, description="Missing 'query' in request data")
+    try:
         res = qa(query)
-        answer, docs = res['result'], [] if args.hide_source else res['source_documents']
-        end = time.time()
-
-        # Print the result
-        print("\n\n> Question:")
-        print(query)
-        print(f"\n> Answer (took {round(end - start, 2)} s.):")
-        print(answer)
-
-        # Print the relevant sources used for the answer
-        for document in docs:
-            print("\n> " + document.metadata["source"] + ":")
-            print(document.page_content)
-
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='privateGPT: Ask questions to your documents without an internet connection, '
-                                                 'using the power of LLMs.')
-    parser.add_argument("--hide-source", "-S", action='store_true',
-                        help='Use this flag to disable printing of source documents used for answers.')
-
-    parser.add_argument("--mute-stream", "-M",
-                        action='store_true',
-                        help='Use this flag to disable the streaming StdOut callback for LLMs.')
-
-    return parser.parse_args()
-
+        answer = res['result']
+        return jsonify({'answer': answer})
+    except Exception as e:
+        abort(500, description=str(e))
 
 if __name__ == "__main__":
-    main()
+    app.run(host='0.0.0.0', port=8000)
